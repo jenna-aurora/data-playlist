aaeecd34d5240f2a254510b4c4e33fd775858a04
jenna-aurora <jenna.a.cardenas@outlook.com>
2026-09-03 01:36:53 -0500

    fix index, closes #1

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
 console.log("Hello World");
 
-let button = document.getElementById("song-button");
-let artistDisplay1 = document.getElementById("artist-1");
-let trackNameDisplay1 = document.getElementById("track-name-1")
+
+/*let button = document.getElementById("song-button");
+
+
+async function loadSongs() {
+  let response = await fetch("/api/v1/datasets/practice-chart/records?limit=3");
+  let data = await response.json();
+  let songs = data.records;
+  let song = songs[0];
+
+  console.log(songs);
+
+  document.getElementById("track-name").textContent = song["Track Name"];
+  document.getElementbyId("track-facts").textContent = "#" + song.Position + "-"
+ + song.Artist;
+}
+
+ button.addEventListener("click", function () {
+  loadSongs();
+});*/
+
+let index = 0;
 
 async function loadSongs() {
   let response = await fetch("https://student-data-api.jenna-a-cardenas.workers.dev/api/v1/datasets/viral-50-usa/records?limit=10");
   console.log("Status: " + response.status);
   let data = await response.json();
   let songs = data.records;
-  let song =songs[0];
 
-  /* console.log(songs); */
+  console.log(songs);
+  
+  let song = songs[index];
+  
   document.getElementById("track-name").textContent = song["Track Name"];
   document.getElementById("track-facts").textContent = "#" + song.Position + " - " + song.Artist;
-  console.log(songs[8]);
-  console.log(songs[8].Artist);
-  console.log(songs[8]["Track Name"]);
-  artistDisplay1.textContent = songs[4].Artist;
-  trackNameDisplay1.textContent - songs[4].Track-Name;
+  
+ 
+  index++;
+  if (index >= songs.length) {
+	  index = 0;
+  }
 }
 
+let button = document.getElementById("song-button");
+	
 button.addEventListener("click", function () {
 	loadSongs();
 });
